fc78078fcf45073dd18d34f9b31b2fbd5c9a1fbf
ganesh-harale1234 <haraleganesh@daccess.co>
2026-09-11 12:09:08 +0530

    new requrement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,10 @@
 export const environment = {
   production: true,
  
-          HostUrl:'http://14.139.120.71:9096/' //prasant .... DY Patil
+          // HostUrl:'http://14.139.120.71:9096/' //prasant .... DY Patil
+                    HostUrl:'http://192.168.100.3:9096/' //Cdaclive .... DY Patil
+
+          // http://192.168.100.3:9096
                     // HostUrl:'http://10.10.30.22:9096/' //prasant ....
 
                     // HostUrl:'http://192.168.1.9:9096/' //Ganesh Sir ...
